80adfe7c8cbb613306013948e9d0ba7c6be43c9d
Exulten <stem.danieljohnbucad@gmail.com>
2024-11-19 23:35:49 +0800

    admin.css location changes
    added copy of admin.css to assets/css

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/0_assets/admin/admin.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/admin.css') }}">
 </head>
 <body>
     @auth
