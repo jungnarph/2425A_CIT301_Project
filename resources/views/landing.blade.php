90bc224ae0c79d797ed6e60b642b3b0208121e6d
Exulten <stem.danieljohnbucad@gmail.com>
2024-11-20 19:36:27 +0800

    [jared] Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EasyCars | Home Page</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
