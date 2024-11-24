333710e5a7f33bd8cdc3a4dc82e2cabb0709942b
Exulten <stem.danieljohnbucad@gmail.com>
2024-11-24 14:27:12 +0800

    Admin rework part 2

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -11,3 +11,21 @@ $(document).ready(function(){
         window.location.href = "{{ route('manage.carmodel') }}";
     }
 });
+
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSubMenu(button) {
+    if (!button.nextElementSibling.classList.contains('show')){
+        closeAllSubMenus();
+    }
+    button.nextElementSibling.classList.toggle('show')
+    button.classList.toggle('rotate')
+}
+
+function closeAllSubMenus() {
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+}
