db2d0ff9a846dd1d4585703e18faf5db666b5c5d
Exulten <stem.danieljohnbucad@gmail.com>
2024-11-24 15:07:30 +0800

    Update 2024_11_24_063243_create_reservations_table.php

--- database/migrations/2024_11_24_063243_create_reservations_table.php
+++ database/migrations/2024_11_24_063243_create_reservations_table.php
@@ -6,22 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->Increments('transaction_id'); // Primary key
+            $table->unsignedInteger('user_id'); // Foreign key
+            $table->unsignedBigInteger('car_id'); // Foreign key
+            $table->date('pickup_date'); 
+            $table->time('pickup_time'); 
+            $table->string('pickup_location'); 
+            $table->date('return_date'); 
+            $table->time('return_time'); 
+            $table->string('return_location'); 
+            $table->string('status'); 
+
+            // Foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
+
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('reservations');
+        Schema::table('reservations', function (Blueprint $table) {
+        });
     }
 };
