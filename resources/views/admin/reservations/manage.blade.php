cd500b3b2f84e5830c029965c0e69d9b8d625c8e
Miguel <166355329+TIPmigs@users.noreply.github.com>
2024-11-29 11:53:30 +0800

    Added reject function

--- resources/views/admin/reservations/manage.blade.php
+++ resources/views/admin/reservations/manage.blade.php
@@ -56,7 +56,7 @@ Reservation Management
                                 </button>
                             </form>
                             @if ($reservation->status === 'Pending')
-                            <form action="{{ route('accept.reservation', $reservation->id) }}" method="POST" style="display:inline;">
+                            <form action="{{ route('assign.reservation', $reservation->id) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('GET')
                                 <button class="success" type="submit">
@@ -68,9 +68,9 @@ Reservation Management
                                 </button>
                             </form>
 
-                            <form action="{{ route('reject.reservation', $reservation->id) }}" method="POST" style="display:inline;">
+                            <form action="{{ route('cancel.reservation', $reservation->id) }}" method="POST" style="display:inline;">
                                 @csrf
-                                @method('GET')
+                                @method('PUT')
                                 <button class="danger" type="submit">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
