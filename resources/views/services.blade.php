0cc6dc6732ef477a63a696cf329fd528eeccb6ca
Exulten <stem.danieljohnbucad@gmail.com>
2024-11-30 18:50:09 +0800

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -17,9 +17,9 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="{{ asset('assets/images/project-logo-transparent.png') }}" alt="Logo" width="auto" height="75">
+                <img src="{{ asset('assets/images/project-logo-rectangle.png') }}" alt="Logo" width="auto" height="75">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
